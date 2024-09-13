05bbdeea26ee848047be774254215190ca442226
mykhailoKalika <mykhailo.kalika@swansoftwaresolutions.com>
2024-09-13 16:49:12 +0300

    Remove unused section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import {BrowserModule} from "@angular/platform-browser";
 import {HeaderSectionComponent} from "./sections/header-section/header-section.component";
 import {MainDescriptionSectionComponent} from "./sections/main-description-section/main-description-section.component";
 import {PricesSectionComponent} from "./sections/prices-section/prices-section.component";
-import {HowDoWeWorkSectionComponent} from "./sections/how-do-we-work-section/how-do-we-work-section.component";
 import {NumbersSectionComponent} from "./sections/numbers-section/numbers-section.component";
 import {PopularQuestionsComponent} from "./sections/popular-questions/popular-questions.component";
 import {WaitingComponent} from "./sections/waiting/waiting.component";
 import {ReviewsComponent} from "./sections/reviews/reviews.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FooterComponent} from "./sections/footer/footer.component";
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,11 +23,11 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     HeaderSectionComponent,
     MainDescriptionSectionComponent,
     PricesSectionComponent,
-    HowDoWeWorkSectionComponent,
     NumbersSectionComponent,
     ReviewsComponent,
     PopularQuestionsComponent,
     WaitingComponent,
+    FooterComponent,
   ],
   bootstrap: [AppComponent],
 })
